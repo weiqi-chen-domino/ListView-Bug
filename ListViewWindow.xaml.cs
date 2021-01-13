4f5c30a900a8d7c84a501ab0991cbeef7953f15e
Weiqi Chen <weiqi.chen@domino.com.cn>
2021-01-13 15:49:15 +0800

    Add project files.

---
+++ ListViewWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for ListViewWindow.xaml
+    /// </summary>
+    public partial class ListViewWindow : Window
+    {
+        public ListViewWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        public void ListViewWindow_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (this.Visibility != Visibility.Visible)
+            {
+                if((sender as ObservableCollection<string>).Count>0)
+                    this.Show();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string current = (string)listView.SelectedItem;
+            (listView.ItemsSource as ObservableCollection<string>).Remove(current);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class Binding : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int num = 0;
+        public int Num 
+        { get=>num;
+            set {
+                num = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("NextListViewItem"));
+            }
+        } 
+        public ObservableCollection<string> Datas { get; } = new ObservableCollection<string>();
+        public string NextListViewItem => string.Format("{0,10:D16}", Num);   
+    }
+
+    public partial class MainWindow : Window
+    {
+        int num = 0;
+        Binding binding = new Binding();
+        ListViewWindow listViewWindow = new ListViewWindow();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            listViewWindow.DataContext = binding;
+            this.DataContext = binding;
+            binding.Datas.CollectionChanged += listViewWindow.ListViewWindow_CollectionChanged;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            binding.Datas.Add(binding.NextListViewItem);
+            binding.Num += 1;
+        }
+    }
+}
